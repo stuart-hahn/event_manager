3294817d9b67e6c628eee6d7750a1a8be60efeb0
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-03 19:01:22 -0400

    use File functionality to parse CSV manually

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,13 @@
 puts 'Event Manager Initialized'
+
+# if File.exist?("event_attendees.csv")
+#   contents = File.read('event_attendees.csv')
+# end
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index| 
+  next if index == 0
+  columns = line.split(',')
+  first_name = columns[2]
+  p first_name
+end
