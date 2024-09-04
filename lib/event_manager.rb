dbdf1420e578194d3c4174f30f56ae3e84b95652
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-03 20:05:56 -0400

    move legislators lookup to its own method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,27 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = File.read('secret.key').strip
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(", ")
+  
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 puts 'EventManager initialized.'
@@ -14,23 +30,14 @@ contents = CSV.open(
   'event_attendees.csv',
   headers: true,
   header_converters: :symbol
-)
-
-contents.each do |row|
-  name = row[:first_name]
-
-  zipcode = clean_zipcode(row[:zipcode])
-
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-  rescue
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-  end
-
+  )
+  
+  contents.each do |row|
+    name = row[:first_name]
+    
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
+    
+    
   puts "#{name} #{zipcode} #{legislators}"
 end
